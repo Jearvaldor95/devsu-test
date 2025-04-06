af7f8036a7acf28d23b291c6099dfc12e37d110f
Jesús Valeta <jvaleta1995@gmail.com>
2025-04-05 23:15:21 -0500

    exception handling

---
+++ customer-service/src/main/java/com/devsu/customer_service/config/GlobalExceptionHandler.java
@@ -0,0 +1,66 @@
+package com.devsu.customer_service.config;
+
+import com.devsu.customer_service.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handlerAlreadyExistsException(CustomerAlreadyExistsException exception){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.CONFLICT.value())
+                .error("CONFLICT")
+                .message(exception.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlerNotFoundException(CustomerNotFoundException exception){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .error("NOT_FOUND")
+                .message(exception.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CustomerConflictException.class)
+    public ResponseEntity<ErrorResponse> handlerConflictException(CustomerConflictException exception){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.CONFLICT.value())
+                .error("CONFLICT")
+                .message(exception.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler({
+            CustomerBadRequestException.class,
+            org.springframework.dao.DuplicateKeyException.class,
+            org.springframework.web.HttpRequestMethodNotSupportedException.class,
+            org.springframework.web.bind.MethodArgumentNotValidException.class,
+            org.springframework.web.bind.MissingRequestHeaderException.class,
+            org.springframework.web.bind.MissingServletRequestParameterException.class,
+            org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class,
+            org.springframework.http.converter.HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<ErrorResponse> handlerBadRequestException(Exception exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .error("BAD_REQUEST")
+                .message(exception.getMessage()) // Capture the generic exception message
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> internalException(Exception exception){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .error("INTERNAL_SERVER_ERROR")
+                .message(exception.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ customer-service/src/main/java/com/devsu/customer_service/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.devsu.customer_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private Integer statusCode;
+    private String error;
+    private String message;
+}
