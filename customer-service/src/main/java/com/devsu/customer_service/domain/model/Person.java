a464789ca1f1a90d81129295ba84b66d9b0d21ed
Jesús Valeta <jvaleta1995@gmail.com>
2025-04-06 09:41:16 -0500

    modification domain person and customer

--- customer-service/src/main/java/com/devsu/customer_service/domain/model/Person.java
+++ customer-service/src/main/java/com/devsu/customer_service/domain/model/Person.java
@@ -1,8 +1,12 @@
 package com.devsu.customer_service.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private Integer personId;
     private String name;
