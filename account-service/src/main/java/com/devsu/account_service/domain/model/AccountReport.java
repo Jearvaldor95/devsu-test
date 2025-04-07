4e0d87b50a64fb2706dbc18f3963ff68ac139fbf
Jesús Valeta <jvaleta1995@gmail.com>
2025-04-07 12:59:37 -0500

    create account report model

---
+++ account-service/src/main/java/com/devsu/account_service/domain/model/AccountReport.java
@@ -0,0 +1,18 @@
+package com.devsu.account_service.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountReport {
+    private Account account;
+    private List<Movement> movements;
+}
